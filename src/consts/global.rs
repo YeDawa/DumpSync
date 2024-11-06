d1dadf79fcd56b1f2c60507b84794e7da5d2b1e5
Kremilly <contact@kremilly.com>
2024-11-06 09:50:39 -0300

    refactor: change url base of default configs file

--- src/consts/global.rs
+++ src/consts/global.rs
@@ -8,7 +8,7 @@ impl Global {
     pub const APP_LICENSE: &'static str = env!("CARGO_PKG_LICENSE");
     pub const APP_HOMEPAGE: &'static str = env!("CARGO_PKG_HOMEPAGE");
 
-    pub const APP_CONFIGS: &'static str = "https://gist.githubusercontent.com/Kremilly/0189fecf4fe7d1b86f35beb8e14f6091/raw/dc34fc70411afbcee76d8afef84739c25d291654/dumpsync.yml";
+    pub const APP_CONFIGS: &'static str = "https://raw.githubusercontent.com/Kremilly/DumpSync/refs/heads/main/dumpsync.yml";
 
     pub fn app_config() -> String {
         format!("{}.yml", Self::APP_NAME)
