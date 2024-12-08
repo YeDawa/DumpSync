12ce3320d78b8ada38551cc958769f48adb035b1
Kremilly <contact@kremilly.com>
2024-12-08 09:13:41 -0300

    fix: update XSS detection regex URL to point to the latest patterns file

--- src/consts/global.rs
+++ src/consts/global.rs
@@ -9,7 +9,7 @@ impl Global {
     pub const APP_HOMEPAGE: &'static str = env!("CARGO_PKG_HOMEPAGE");
 
     pub const APP_CONFIGS: &'static str = "https://raw.githubusercontent.com/Kremilly/DumpSync/refs/heads/main/dumpsync.yml";
-    pub const XSS_DETECT_REGEX: &'static str = "https://gist.githubusercontent.com/Kremilly/caa28b398966390730a4311337026a0f/raw/15efbaf184d7e3ee3c65cbf79de1f9f1f5425a27/xss-patterns.dsp";
+    pub const XSS_DETECT_REGEX: &'static str = "https://raw.githubusercontent.com/Kremilly/DumpSync/refs/heads/main/patterns.txt";
 
     pub fn app_config() -> String {
         format!("{}.yml", Self::APP_NAME)
