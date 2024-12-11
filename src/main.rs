7863a3221efa3a7009d7a1f2c0953e3c6cc2c850
Kremilly <contact@kremilly.com>
2024-12-10 23:06:11 -0300

    refactor: simplify error handling by using a more specific error type in main function

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,12 @@ mod handlers;
 mod args_cli;
 mod dump_sync;
 
+use std::error::Error;
+
 use crate::dump_sync::DumpSync;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), Box<dyn Error>> {
     if let Err(e) = DumpSync.init().await {
         eprintln!("Error initializing app: {}", e);
         return Err(e);
