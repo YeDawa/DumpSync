a38339d91469b1cdab660360dc4c33821322ba7d
Kremilly <contact@kremilly.com>
2025-01-29 13:27:55 -0300

    refactor: optimize random string generation by using a custom charset

--- src/utils/generate.rs
+++ src/utils/generate.rs
@@ -1,17 +1,18 @@
-use rand::{
-    Rng,
-    distributions::Alphanumeric,
-};
+use rand::Rng;
 
 pub struct Generate;
 
 impl Generate {
 
     pub fn random_string(&self, size: usize) -> String {
-        let mut rng = rand::thread_rng();
+        let charset = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        let mut rng = rand::rng();
         
         (0..size)
-            .map(|_| rng.sample(Alphanumeric) as char)
+            .map(|_| {
+                let idx = rng.random_range(0..charset.len());
+                charset[idx] as char
+            })
             .collect()
     }
 
