a5dfba54746db092a1ee9321e5d6f24769400b43
Kremilly <contact@kremilly.com>
2025-02-04 08:14:54 -0300

    refactor: enhance PDF report generation by adding YAML configuration reading

--- src/plugins/reports_pdf.rs
+++ src/plugins/reports_pdf.rs
@@ -1,8 +1,12 @@
 use printpdf::*;
 
 use std::{
-    fs::File,
     io::BufWriter,
+
+    fs::{
+        self, 
+        File
+    },
 };
 
 use crate::{
@@ -24,70 +28,78 @@ impl ReportsPdfs {
         Self {
             file: file.to_string(),
             path: path.to_string(),
-
             interval,
             counter,
         }
     }
 
-    fn add_text(&self, layer: &PdfLayerReference, text: &str, y: &mut Mm, margin_left: &Mm, font: &IndirectFontRef, line_height: &Mm, size: f32) {
+    fn read_yaml_as_text(&self) -> String {
+        fs::read_to_string(Global::app_config()).expect("Error reading the YAML file")
+    }
+
+    fn add_text(
+        &self,
+        doc: &mut PdfDocumentReference,
+        current_page: &mut PdfPageIndex,
+        current_layer: &mut PdfLayerIndex,
+        text: &str,
+        y: &mut Mm,
+        margin_left: &Mm,
+        font: &IndirectFontRef,
+        line_height: &Mm,
+        size: f32,
+    ) {
+        if *y < Mm(20.0) {
+            let (new_page, new_layer) = doc.add_page(Mm(210.0), Mm(297.0), "New page");
+            *current_page = new_page;
+            *current_layer = new_layer;
+            *y = Mm(280.0);
+        }
+
+        let layer = doc.get_page(*current_page).get_layer(*current_layer);
         layer.use_text(text, size, *margin_left, *y, font);
+
         *y -= *line_height;
     }
 
     pub fn dump(&self) {
-        let reports = ReportsHandlers::new(
-            &self.path,
-            self.interval,
-            self.counter,
-            None
-        );
+        let reports = ReportsHandlers::new(&self.path, self.interval, self.counter, None);
 
-        let (doc, page1, layer1) = PdfDocument::new("Report dumps", Mm(210.0), Mm(297.0), "Camada 1");
-        let current_layer = doc.get_page(page1).get_layer(layer1);
+        let (mut doc, mut current_page, mut current_layer) =
+            PdfDocument::new("Report dumps", Mm(210.0), Mm(297.0), "Layer 1");
 
         let font = doc.add_builtin_font(BuiltinFont::Helvetica).unwrap();
         let font_header = doc.add_builtin_font(BuiltinFont::CourierBold).unwrap();
 
+        let mg_top = Mm(280.0);
+        let mg_left = Mm(10.0);
         let line_height = Mm(8.0);
-        let margin_top = Mm(280.0);
-        let margin_left = Mm(10.0);
-        let mut y_position = margin_top;
+        let mut y_position = mg_top;
 
-        let x_pos = Mm(10.0);
-        let y_pos = Mm(20.0);
-
-        let link_text = format!("Generated using {}", Global::APP_NAME);
-        current_layer.use_text(link_text, 16.0, x_pos, y_pos, &font);
-
-        let link_annotation = LinkAnnotation::new(
-            printpdf::Rect::new(x_pos, y_pos - Mm(2.0), x_pos + Mm(100.0), y_pos + Mm(8.0)), // Define a área clicável
-            Some(printpdf::BorderArray::default()),
-            Some(printpdf::ColorArray::default()),
-            printpdf::Actions::uri(Global::APP_HOMEPAGE.to_string()),
-            Some(printpdf::HighlightingMode::Invert),
-        );
-    
-        current_layer.add_link_annotation(link_annotation);
-
-        self.add_text(&current_layer, "Final report", &mut y_position, &margin_left, &font_header, &line_height, 12.0);
-
-        self.add_text(&current_layer, &format!("Path: {}", &self.path), &mut y_position, &margin_left, &font, &line_height, 8.0);
-        self.add_text(&current_layer, &format!("Interval: {} seconds", &self.interval), &mut y_position, &margin_left, &font, &line_height, 8.0);
-        self.add_text(&current_layer, &format!("Total number of dumps: {}", &self.counter), &mut y_position, &margin_left, &font, &line_height, 8.0);
+        self.add_text(&mut doc, &mut current_page, &mut current_layer, "Final report", &mut y_position, &mg_left, &font_header, &line_height, 12.0);
+        self.add_text(&mut doc, &mut current_page, &mut current_layer, &format!("Path: {}", &self.path), &mut y_position, &mg_left, &font, &line_height, 8.0);
+        self.add_text(&mut doc, &mut current_page, &mut current_layer, &format!("Interval: {} seconds", &self.interval), &mut y_position, &mg_left, &font, &line_height, 8.0);
+        self.add_text(&mut doc, &mut current_page, &mut current_layer, &format!("Total number of dumps: {}", &self.counter), &mut y_position, &mg_left, &font, &line_height, 8.0);
 
         if let Some((last_dump, size)) = reports.get_most_recent_sql_file(&self.path) {
-            self.add_text(&current_layer, &format!("Last dump: {} ({})", last_dump, size), &mut y_position, &margin_left, &font, &line_height, 8.0);
+            self.add_text(&mut doc, &mut current_page, &mut current_layer, &format!("Last dump: {} ({})", last_dump, size), &mut y_position, &mg_left, &font, &line_height, 8.0);
 
             if let Some(tables) = reports.extract_table_names(&last_dump) {
-                self.add_text(&current_layer, "Tables dumped:", &mut y_position, &margin_left, &font_header, &line_height, 12.0);
+                self.add_text(&mut doc, &mut current_page, &mut current_layer, "Tables dumped:", &mut y_position, &mg_left, &font_header, &line_height, 12.0);
 
                 for table in tables {
-                    self.add_text(&current_layer, &format!("- {}", table), &mut y_position, &margin_left, &font, &line_height, 8.0);
+                    self.add_text(&mut doc, &mut current_page, &mut current_layer, &format!("- {}", table), &mut y_position, &mg_left, &font, &line_height, 8.0);
                 }
             } else {
-                self.add_text(&current_layer, "No tables found in the dump.", &mut y_position, &margin_left, &font, &line_height, 8.0);
+                self.add_text(&mut doc, &mut current_page, &mut current_layer, "No tables found in the dump.", &mut y_position, &mg_left, &font, &line_height, 8.0);
             }
+        }
+
+        self.add_text(&mut doc, &mut current_page, &mut current_layer, "Settings:", &mut y_position, &mg_left, &font_header, &line_height, 12.0);
+        
+        for line in self.read_yaml_as_text().lines() {
+            if line.is_empty() { continue }
+            self.add_text(&mut doc, &mut current_page, &mut current_layer, line, &mut y_position, &mg_left, &font, &line_height, 8.0);
         }
 
         let mut pdf = BufWriter::new(File::create(&self.file).expect("Error creating PDF"));
