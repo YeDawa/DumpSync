fef341bde4785b4e2014f7456b81f0a8e37d5c3b
Kremilly <contact@kremilly.com>
2025-03-26 23:07:27 -0300

    refactor: remove unnecessary blank lines in DumpSync init method

--- src/dump_sync.rs
+++ src/dump_sync.rs
@@ -15,11 +15,12 @@ impl DumpSync {
     pub async fn init(&self) -> Result<(), Box<dyn Error>> {
         match Cli::parse().command {
             Commands::Init => DumpSyncInit.initialize().await?,
-            
+
             Commands::Export(options) => DumpSyncDumper.export(options),
             Commands::Import(options) => DumpSyncDumper.import(options),
             Commands::Transfer(options) => DumpSyncDumper.transfer(options),
             Commands::Truncate(options) => DumpSyncDumper.truncate(options),
+            
             Commands::Schema(options) => DumpSyncAddons.schema(options)?,
             Commands::Checksum(options) => DumpSyncAddons.checksum(options),
             Commands::Visual(options) => DumpSyncAddons.visual(options).await,
