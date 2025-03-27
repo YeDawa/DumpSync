47719cc32e193284cf12b268f72ee50adc6641d1
Kremilly <contact@kremilly.com>
2025-03-26 23:06:20 -0300

    refactor: reorder Checksum command in DumpSync implementation

--- src/dump_sync.rs
+++ src/dump_sync.rs
@@ -19,10 +19,10 @@ impl DumpSync {
             Commands::Export(options) => DumpSyncDumper.export(options),
             Commands::Import(options) => DumpSyncDumper.import(options),
             Commands::Transfer(options) => DumpSyncDumper.transfer(options),
-            Commands::Checksum(options) => DumpSyncAddons.checksum(options),
             Commands::Truncate(options) => DumpSyncDumper.truncate(options),
-
+            
             Commands::Schema(options) => DumpSyncAddons.schema(options)?,
+            Commands::Checksum(options) => DumpSyncAddons.checksum(options),
             Commands::Visual(options) => DumpSyncAddons.visual(options).await,
             Commands::Share(options) => DumpSyncAddons.share(options).await?,
             Commands::Scan(options) => DumpSyncAddons.scan_xss(options).await?,
