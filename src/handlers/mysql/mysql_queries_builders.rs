6649d8eb0ee2f01eaa6c9bf82c89fdc3703fc370
Kremilly <contact@kremilly.com>
2025-10-11 14:07:38 -0300

    feat: enhance MySQL keyword handling in queries and builders

--- src/handlers/mysql/mysql_queries_builders.rs
+++ src/handlers/mysql/mysql_queries_builders.rs
@@ -1,115 +1,115 @@
+use crate::handlers::mysql::mysql_keywords::MySQLKeywords;
+
 pub struct MySqlQueriesBuilders;
 
 impl MySqlQueriesBuilders {    
 
     pub fn show_tables(&self) -> String {
-        "SHOW TABLES".to_string()
+        MySQLKeywords::ShowTables.as_str().to_string()
     }
 
     pub fn lock_tables(&self, table: &str) -> String {
-        format!("LOCK TABLE `{}`;", table)
+        format!("{} `{}`;", MySQLKeywords::LockTables.as_str(), table)
     }
 
     pub fn unlock_tables(&self, table: &str) -> String {
-        format!("UNLOCK TABLE `{}`;", table)
+        format!("{} `{}`;", MySQLKeywords::UnlockTables.as_str(), table)
     }
 
     pub fn truncate_table(&self, table: &str) -> String {
-        format!("TRUNCATE TABLE `{}`;", table)
+        format!("{} `{}`;", MySQLKeywords::TruncateTable.as_str(), table)
     }
 
     pub fn show_create_table(&self, table: &str) -> String {
-        format!("SHOW CREATE TABLE `{}`;", table)
+        format!("{} `{}`;", MySQLKeywords::ShowCreateTable.as_str(), table)
     }
 
     pub fn drop_table(&self, table: &str) -> String {
-        format!("DROP TABLE IF EXISTS `{}`;", table)
+        format!("{} {} `{}`;", MySQLKeywords::DropTable.as_str(), MySQLKeywords::IfExists.as_str(), table)
     }
 
     pub fn create_database_not_exists(&self, dbname: &str) -> String {
-        format!("CREATE DATABASE IF NOT EXISTS `{}`;", dbname)
+        format!("{} {} `{}`;", MySQLKeywords::CreateDatabase.as_str(), MySQLKeywords::IfNotExists.as_str(), dbname)
     }
 
     pub fn create_database(&self, dbname: &str) -> Result<(String, String), String> {
-        let create_db = format!("CREATE DATABASE IF NOT EXISTS `{}`;\n", dbname);
-        let use_db = format!("USE `{}`;", dbname);
+        let create_db = format!(
+            "{} {} `{}`;\n", MySQLKeywords::CreateDatabase.as_str(), MySQLKeywords::IfNotExists.as_str(), dbname
+        );
 
+        let use_db = format!("{} `{}`;", MySQLKeywords::Use.as_str(), dbname);
         Ok((create_db, use_db))
     }
 
     pub fn select(&self, table: &str, offset: Option<usize>, limit: Option<usize>) -> String {
-        let mut query = format!("SELECT * FROM `{}`", table);
+        let mut query = format!("{} `{}`", MySQLKeywords::SelectFrom.as_str(), table);
 
         if let Some(l) = limit {
-            query.push_str(&format!(" LIMIT {}", l));
+            query.push_str(&format!(" {} {}", MySQLKeywords::Limit.as_str(), l));
         }
 
         if let Some(o) = offset {
-            query.push_str(&format!(" OFFSET {}", o));
+            query.push_str(&format!(" {} {}", MySQLKeywords::Offset.as_str(), o));
         }
 
         query
     }
 
     pub fn get_table_names(&self) -> String {
-        "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = DATABASE();".to_string()
+        format!("{};", MySQLKeywords::GetTableNames.as_str())
+    }
+
+    pub fn show_columns(&self, table: &str) -> String {
+        format!("{} `{}`;", MySQLKeywords::ShowColumns.as_str(), table)
     }
 
     pub fn table_info(&self, table: &str) -> String {
-        format!(
-            r#"
-            SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY
-            FROM INFORMATION_SCHEMA.COLUMNS
-            WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = '{}';
-            "#,
-            table
-        )
+        format!("{} = '{}'", MySQLKeywords::TableInfo.as_str(), table)
     }
 
     pub fn foreign_key_info(&self, table: &str) -> String {
-        format!(
-            r#"
-            SELECT COLUMN_NAME, REFERENCED_TABLE_NAME, REFERENCED_COLUMN_NAME
-            FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
-            WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = '{}' AND REFERENCED_TABLE_NAME IS NOT NULL;
-            "#,
-            table
-        )
+        format!("{} = '{}' {};", MySQLKeywords::ForeignKeyInfo.as_str(), table, MySQLKeywords::AndReferencedIsNotNull.as_str())
     }
 
     pub fn get_alter_table(&self, table: &str) -> String {
-        format!(
-            r#"
-            SELECT CONSTRAINT_NAME, COLUMN_NAME, REFERENCED_TABLE_NAME, REFERENCED_COLUMN_NAME 
-            FROM information_schema.KEY_COLUMN_USAGE 
-            WHERE TABLE_NAME = DATABASE() AND TABLE_SCHEMA = '{}'
-            AND REFERENCED_TABLE_NAME IS NOT NULL
-            "#,
-            table
-        )
+        format!("{} = '{}' {}", MySQLKeywords::GetAlterTable.as_str(), table, MySQLKeywords::AndReferencedIsNotNull.as_str())
     }
 
     pub fn get_foreign_keys(&self, table: &str, constraint_name: &str, column_name: &str, ref_table: &str, ref_column: &str) -> String {
         format!(
-            "ALTER TABLE `{}` ADD CONSTRAINT `{}` FOREIGN KEY (`{}`) REFERENCES `{}` (`{}`);",
-            table, constraint_name, column_name, ref_table, ref_column
+            "{} `{}` {} `{}` {} (`{}`) {} `{}` (`{}`);",
+            MySQLKeywords::AlterTable.as_str(),
+            table, 
+            MySQLKeywords::AddConstraint.as_str(),
+            constraint_name, 
+            MySQLKeywords::ForeignKey.as_str(),
+            column_name, 
+            MySQLKeywords::References.as_str(),
+            ref_table, 
+            ref_column
         )
     }
     
     pub fn get_unique_keys(&self, table: &str, constraint_name: &str, column_name: &str) -> String {
         format!(
-            "ALTER TABLE `{}` ADD CONSTRAINT `{}` UNIQUE (`{}`);",
-            table, constraint_name, column_name
+            "{} `{}` {} `{}` {} (`{}`);",
+            MySQLKeywords::AlterTable.as_str(), 
+            table, 
+            MySQLKeywords::AddConstraint.as_str(), 
+            constraint_name, 
+            MySQLKeywords::Unique.as_str(), 
+            column_name
         )
     }
 
-    pub fn show_columns(&self, table: &str) -> String {
-        format!("SHOW COLUMNS FROM `{}`", table)
-    }
-
     pub fn insert_into_start(&self, table: &str, columns: &[String], insert_ignore: bool) -> String {
-        let cmd = if insert_ignore { "INSERT IGNORE INTO" } else { "INSERT INTO" };
-        format!("{} `{}` ({}) VALUES ", cmd, table, columns.join(", "))
+        let cmd = if insert_ignore { 
+            MySQLKeywords::InsertIgnore.as_str() 
+        } else { 
+            MySQLKeywords::InsertInto.as_str() 
+        };
+
+        format!("{} `{}` ({}) {}", cmd, table, columns.join(", "), MySQLKeywords::Values.as_str())
     }
     
 }
