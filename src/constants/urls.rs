ba1a80e1510cd0fcde1491893d1ba9a1b875ad62
Kremilly <contact@kremilly.com>
2025-10-11 15:25:06 -0300

    fix: remove unnecessary comment in Urls struct

--- src/constants/urls.rs
+++ src/constants/urls.rs
@@ -12,7 +12,5 @@ impl Urls {
     pub const DUMPSYNC_API_KEY: &'static str = "https://dumpsync.com/dashboard/settings/api-key";
 
     pub const DOWNLOAD_FILES_URI: &'static str = "https://raw.githubusercontent.com/YeDawa/DumpSync/main/";
-
-    // DumpSync Package Manager
     
 }
